4c589aae2c286f5dc3420bac793376298daba518
minho2618 <minho2618@naver.com>
2025-05-17 00:53:46 +0900

    Init Setup

---
+++ src/test/java/com/lmh/cloneshops/CloneShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmh.cloneshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneShopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
